01ea0783586962c3a127f452743abdaeb03bccd6
OzuAkira <22311038ao@tama.ac.jp>
2025-09-25 23:18:20 +0900

    色々やった
    ＊プレイヤーの復活
    ＊復活時の残機減少とボムの初期化
    ＊残機、ボムUIと内部変数を紐づけた
    ＊復活時の無敵時間を追加

--- Assets/script/all_enemy/enemy_bullet.cs
+++ Assets/script/all_enemy/enemy_bullet.cs
@@ -2,26 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy_a_bullet : MonoBehaviour
+public class enemy_bullet : MonoBehaviour
 {
-    //このスクリプトで描くこと↓
-
-    //プレイヤーに弾が当たった時の処理
-    //弾の起動
-
-    //intervalは描かない！
-
-    Rigidbody2D rb;
     [SerializeField] float moveSpeed;
     [SerializeField] int destroy_Frame;
+    GameObject GM;
     int i;
     bool wall_flag;
 
-    private void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        GM = GameObject.Find("GameMaster");
     }
-
     void Update()
     {
         if (wall_flag)
@@ -29,15 +21,19 @@ public class enemy_a_bullet : MonoBehaviour
             i++;
             if (destroy_Frame <= i)Destroy(gameObject);//壁にdestroy_flam間触れていたら消す
         }
-        rb.velocity = new Vector2(0, moveSpeed);//真下に動かす 
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             GameObject player = collision.gameObject;
-            player.SetActive(false);
-            Debug.Log("miss!!");
+            Resurrection resu = GM.GetComponent<Resurrection>();
+
+            if (resu.isResurrection == false)
+            {
+                player.SetActive(false);
+                Debug.Log("miss!!");
+            }
         }
         else if (collision.CompareTag("wall"))
         {

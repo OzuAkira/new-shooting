735fd1a4f4adb342c6ed182efddeb3dff9567489
OzeAkira <22311038ao@tama.ac.jp>
2025-09-25 15:14:26 +0900

    途中

---
+++ Assets/script/enemy_a/enemy_a_bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_a_bullet : MonoBehaviour
+{
+    //このスクリプトで描くこと↓
+
+    //プレイヤーに弾が当たった時の処理
+    //弾の起動
+
+    //intervalは描かない！
+    void Update()
+    {
+
+    }
+}

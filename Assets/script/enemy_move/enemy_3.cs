6ac7b01355b9a4afcbbad62c14dc8822ffae5c64
OzeAkira <22311038ao@tama.ac.jp>
2025-09-29 16:26:57 +0900

    敵の挙動を作成
    enemy_1～3　まで

---
+++ Assets/script/enemy_move/enemy_3.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_3 : MonoBehaviour
+{
+    Vector3 myPos;
+    Rigidbody2D rb;
+    void Start()
+    {
+        
+    }
+    IEnumerator move()
+    {
+        yield return null;
+    }
+}

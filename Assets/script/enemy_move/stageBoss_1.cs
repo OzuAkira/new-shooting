6102372f07d05163ce4e16aab2049aa3eb41270a
OzuAkira <22311038ao@tama.ac.jp>
2025-10-02 18:01:22 +0900

    ボス戦の内容を検討中
    検討するために検討をし、検討の検討中であります！

---
+++ Assets/script/enemy_move/stageBoss_1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stageBoss_1 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    IEnumerator phase_1()
+    {
+        yield return null;
+    }
+}
